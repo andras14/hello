2346584ff9a049b5a19dadcdd8531c8f643d1ef5
andras <andras.odonnell13@gmail.com>
2018-02-24 13:56:43 +0000

    it's a calculator

---
+++ calc.js
@@ -0,0 +1,19 @@
+
+ // defines the Function
+function doOp(operation, num1, num2){
+ var result;
+if (operation === "add"){
+  result = num1 * num2;
+  
+} else if (operation === "subtract") {
+ result = num1 - num2;
+ 
+}
+ else if (operation === "multiply"){
+result = num1 * num2;
+
+ }return result
+}
+ 
+ 
+
